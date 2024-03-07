d0189bbdc723578eb40c1114ec912f493a48b963
Taisiia Galkina <galkina.tasya@mail.ru>
2024-03-07 15:57:38 +0300

    Initial Commit

---
+++ MentorMatch/MentorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentorMatchApp.swift
+//  MentorMatch
+//
+//  Created by Тася Галкина on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MentorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
